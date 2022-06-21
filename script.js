4bfb801e12733ab452da7d65940a11a95d4532e7
Thomas <thomas-g-smith@hotmail.com>
2022-06-21 14:21:34 -0500

    worked on changing colors

--- script.js
+++ script.js
@@ -11,5 +11,29 @@ function makeRow(){
     }
     table.appendChild(row)
 }
-makeRow()
-makeRow()
+makeRow();
+makeRow();
+
+const button = document.getElementById('add-row');
+button.addEventListener('click', makeRow)
+
+table.addEventListener('click', colorize)
+function colorize(event){
+    const target = event.target
+    if(target.className.length){
+        target.className = ''
+    } else {
+        target.className = 'red'
+    }
+console.log('click')
+}
+
+
+
+const colors = document.getElementsByTagName('select')
+function colorChange(event){
+    colors.addEventListener('change', colorChange)
+     console.log(event.target.value)
+}
+
+
